328f6ba2fc49e40f75afb13f95524ca9ddc89f93
Paulo Henrique <paulo@rocha.com>
2026-02-03 07:36:21 -0300

    Adicionando configuração de fixture para setup e teardown do banco de dados nos testes

--- conftest.py
+++ conftest.py
@@ -1,4 +1,12 @@
 import sys
 import os
+import pytest
+from main import Base, engine
+
+@pytest.fixture(scope="session", autouse=True)
+def setup_database():
+    Base.metadata.create_all(bind=engine)
+    yield
+    Base.metadata.drop_all(bind=engine)
 
 sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
